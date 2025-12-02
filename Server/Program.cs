a7bdbba0f84acff6db32b7cc35ea9898dced3213
Kaktys868 <lastochka1230@yandex.ru>
2025-12-02 16:41:47 +0500

    Накопировал и добавил методы. 41:31. Винокуров

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,15 +1,158 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Server
 {
-    internal class Program
+    class Program
     {
+        static IPAddress ServerIpAddress;
+        static int ServerPort;
+        static int MaxClient;
+        static int Duration;
         static void Main(string[] args)
         {
+            Console.WriteLine("");
         }
+        public static void SetCommand()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            string Command = Console.ReadLine();
+
+            if (Command == "/config")
+            {
+                File.Delete(Directory.GetCurrentDirectory() + "/.config");
+                OnSettings();
+            }
+            else if (Command == "/connect") ConnectServer();
+            else if (Command == "/status") GetStatus();
+            else if (Command == "/help") Help();
+        }
+        public static void GetStatus()
+        {
+            int Duration = (int)DateTime.Now.Subtract(ClientDateConnection).TotalSeconds;
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+        public static void ConnectServer()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("Login: ");
+            string login = Console.ReadLine();
+
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            IPEndPoint endPoint = new IPEndPoint(ServerIpAddress, ServerPort);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(endPoint);
+
+            string msg = $"/connect {login} {password}";
+            socket.Send(Encoding.UTF8.GetBytes(msg));
+
+            byte[] buffer = new byte[1024];
+            int size = socket.Receive(buffer);
+            string response = Encoding.UTF8.GetString(buffer, 0, size);
+
+            if (response == "/auth_fail")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Неверный логин или пароль.");
+                return;
+            }
+        }
+        public static void OnSettings()
+        {
+            string Path = Directory.GetCurrentDirectory() + "/.config";
+            string IpAddress = "";
+
+            if (File.Exists(Path))
+            {
+                StreamReader streamReader = new StreamReader(Path);
+                IpAddress = streamReader.ReadLine();
+                ServerIpAddress = IPAddress.Parse(IpAddress);
+                ServerPort = int.Parse(streamReader.ReadLine());
+                streamReader.Close();
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Server address: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(IpAddress);
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Server port: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(ServerPort.ToString());
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Max count clients: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(MaxClient.ToString());
+
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Token lifetime: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(Duration.ToString());
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Please provide the IP address if the license server: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                IpAddress = Console.ReadLine();
+                ServerIpAddress = IPAddress.Parse(IpAddress);
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Please specify the license server port: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                ServerPort = int.Parse(Console.ReadLine());
+
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Please indicate the largest number of clients: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                MaxClient = int.Parse(Console.ReadLine());
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Specify the token lifetime: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Duration = int.Parse(Console.ReadLine());
+
+                StreamWriter streamWriter = new StreamWriter(Path);
+                streamWriter.WriteLine(IpAddress);
+                streamWriter.WriteLine(ServerPort.ToString());
+                streamWriter.WriteLine(MaxClient.ToString());
+                streamWriter.WriteLine(Duration.ToString());
+                streamWriter.Close();
+            }
+        }
+        public static void Help()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Commands to the server: ");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("/config");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" - set initial settings ");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("/connect");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" - connection to the server ");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("/status");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" - show list users ");
+        }
+
     }
 }
